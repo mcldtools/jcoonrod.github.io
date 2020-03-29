3c8753b35b8c32df77985f5a808ae4a0c5a088e0
jcoonrod <54367582+jcoonrod@users.noreply.github.com>
2020-03-29 18:51:31 -0400

    Update sw.js

--- spider/sw.js
+++ spider/sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'spider_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_127'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_128'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/spider/index.html'            // add path to those files here
